1c7529718b50c04819b45415064bf9776fa166cb
Svetlana Rozanova <rozanovasa@yandex.ru>
2025-02-23 17:29:50 +0300

    2-collector-grpc-v2

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/MainApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/MainApplication.java
@@ -2,12 +2,10 @@ package ru.yandex.practicum.collector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.shell.command.annotation.CommandScan;
 
 @SpringBootApplication
-@CommandScan(basePackages = "ru.practicum.collector")
-@EnableScheduling
+@CommandScan(basePackages = "ru.yandex.practicum.collector")
 public class MainApplication {
 
     public static void main(String[] args) {
