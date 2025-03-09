0616320d3d354ab93ca20f38491a2b0ec7b16702
Svetlana Rozanova <rozanovasa@yandex.ru>
2025-03-09 21:52:36 +0300

    4-analyzer-v20

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerRunner.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/AnalyzerRunner.java
@@ -2,9 +2,12 @@ package ru.yandex.practicum.analyzer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.analyzer.processors.HubEventProcessor;
 import ru.yandex.practicum.analyzer.processors.SnapshotProcessor;
+
+import java.util.concurrent.*;
 
 @Component
 @RequiredArgsConstructor
@@ -14,9 +17,17 @@ public class AnalyzerRunner implements CommandLineRunner {
     private final SnapshotProcessor snapshotProcessor;
 
     public void run(String... args) {
-        Thread hubEventsThread = new Thread(hubEventProcessor);
-        hubEventsThread.setName("HubEventHandlerThread");
-        hubEventsThread.start();
-        snapshotProcessor.start();
+
+        ExecutorService executorservice = getExecutorService();
+        executorservice.submit(hubEventProcessor);
+        executorservice.submit(snapshotProcessor);
     }
+
+    private ExecutorService getExecutorService() {
+        BlockingQueue<Runnable> queue = new ArrayBlockingQueue<>(1000);
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1000, 1000, 60L, TimeUnit.SECONDS, queue, new ThreadPoolExecutor.AbortPolicy());
+        threadPoolExecutor.allowCoreThreadTimeOut(true);
+        return threadPoolExecutor;
+    }
+
 }
