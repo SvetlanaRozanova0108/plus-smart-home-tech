0594a8b0985ef3fb42b77b41e33ad5f5c4fa7e22
Svetlana Rozanova <rozanovasa@yandex.ru>
2025-03-09 19:25:40 +0300

    4-analyzer-v12

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/handlers/HubHandlers.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/analyzer/handlers/HubHandlers.java
@@ -11,10 +11,10 @@ import java.util.stream.Collectors;
 @Component
 public class HubHandlers {
 
-    private final Map<String, HubEventHandler> builders;
+    private final Map<String, HubEventHandler> handlers;
 
-    public HubHandlers(Set<HubEventHandler> builders) {
-        this.builders = builders.stream()
-                .collect(Collectors.toMap(HubEventHandler::getMessageType, b -> b));
+    public HubHandlers(Set<HubEventHandler> handlers) {
+        this.handlers = handlers.stream()
+                .collect(Collectors.toMap(HubEventHandler::getMessageType, h -> h));
     }
 }
