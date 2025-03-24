2d53daeb78ff6a82bf30752cc9777161e1cf6db5
Svetlana Rozanova <rozanovasa@yandex.ru>
2025-03-24 21:46:25 +0300

    7-spring-cloud-microservices-v2

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/interactionapi/feign/WarehouseClient.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/interactionapi/feign/WarehouseClient.java
@@ -3,13 +3,14 @@ package ru.yandex.practicum.interactionapi.feign;
 import jakarta.validation.Valid;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.interactionapi.circuitBreaker.WarehouseClientFallback;
 import ru.yandex.practicum.interactionapi.dto.AddressDto;
 import ru.yandex.practicum.interactionapi.dto.BookedProductsDto;
 import ru.yandex.practicum.interactionapi.dto.ShoppingCartDto;
 import ru.yandex.practicum.interactionapi.request.AddProductToWarehouseRequest;
 import ru.yandex.practicum.interactionapi.request.NewProductInWarehouseRequest;
 
-@FeignClient(name = "warehouse", path = "/api/v1/warehouse")
+@FeignClient(name = "warehouse", path = "/api/v1/warehouse", fallback = WarehouseClientFallback.class)
 public interface WarehouseClient {
 
     @PutMapping
